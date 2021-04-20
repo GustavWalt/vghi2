6bc6898c3ba90cd98001eb3c3175aaaf52b5fa27
Gustav <gustav.walter@elev.ga.ntig.se>
2021-04-20 11:43:06 +0200

    Header component made

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,20 @@
 import React from "react";
-import styles from "../style/modules/navbar/Navbar.module.scss";
+import styles from "../style/modules/header/Header.module.scss";
 
 const Header = () => {
   return (
     <>
-      <h1>hello</h1>
+      <div className={styles.headerBg}>
+        <h1>RSBoosts</h1>
+        <h1 className={styles.displayNone}>Services</h1>
+        <h2>SKILLING, BOSSING, & QUESTING SERVICES</h2>
+        <span />
+        <div className={styles.flex}>
+          <button>Skilling</button>
+          <button>Bossing</button>
+          <button>Questing</button>
+        </div>
+      </div>
     </>
   );
 };
