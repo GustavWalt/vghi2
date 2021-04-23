ed8138bd8e54d610709837b74fee401a9b31efce
Gustav <gustav.walter@elev.ga.ntig.se>
2021-04-23 10:31:17 +0200

    Added new font & meta-tags

---
+++ components/Meta.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+const Meta = () => {
+  return (
+    <>
+      <meta name="language" content="English" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="canonical" href="https://rsboosts.vercel.app/" />
+      <meta
+        name="description"
+        content="RSBoosts.com - Cheap & reliable OSRS boosting service with cheap prices."
+      />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="theme-color" content="#363636" />
+      <meta property="og:title" content="RSBoosts | Cheap & Reliable" />
+      <meta property="og:type" content="web" />
+      <meta property="og:url" content="https://rsboosts.vercel.app/" />
+      <meta
+        property="og:description"
+        content="RSBoosts.com - Cheap & reliable OSRS boosting service with cheap prices."
+      />
+      <meta
+        property="og:image"
+        content="https://rsboosts.vercel.app/header.png"
+      ></meta>
+      <meta name="twitter:card" content="RSBoosts | Cheap & Reliable" />
+      <meta name="twitter:site" content="https://rsboosts.vercel.app/" />
+      <meta name="twitter:title" content="RSBoosts | Cheap & Reliable" />
+      <meta
+        name="twitter:description"
+        content="RSBoosts.com - Cheap & reliable OSRS boosting service with cheap prices."
+      />
+      <meta
+        name="twitter:image"
+        content="https://rsboosts.vercel.app/header.png"
+      ></meta>
+    </>
+  );
+};
+
+export default Meta;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import next from "next";
+import Head from "next/head";
 import Navbar from "../components/Navbar";
 import Header from "../components/Header";
 import Services from "../components/Services";
 import About from "../components/About";
 import BlogPosts from "../components/BlogPosts";
 import Footer from "../components/Footer";
+import Meta from "../components/Meta";
 import { GetStaticProps } from "next";
 import React from "react";
 import prisma from "../lib/prisma";
@@ -17,6 +19,10 @@ export const getStaticProps: GetStaticProps = async () => {
 const homePage = (props) => {
   return (
     <>
+      <Head>
+        <title>RSBoosts | Cheap & Reliable</title>
+        <Meta />
+      </Head>
       <Navbar />
       <Header />
       <Services />
