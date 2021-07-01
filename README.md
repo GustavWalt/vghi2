f34f8e89544661cce229c2f85d0a469d0c5585be
GustavWalt <shardoomtv6957@hotmail.com>
2021-07-01 19:18:11 +0200

    Changed metadata

--- README.md
+++ README.md
@@ -2,9 +2,5 @@
 
 # npx prisma studio - starting prisma studio to visualize data
 
-Skapa informations-sida
-Skapa kunder sida
-Fixa styling på böckerna
 Skapa kontakt sida (eget API)
 Skapa cart design
-Ändra footer content
--- components/assets/Meta.tsx
+++ components/assets/Meta.tsx
@@ -28,35 +28,20 @@ const Meta = () => {
       />
       <link rel="manifest" href="/site.webmanifest" />
       <link rel="canonical" href="https://www.rsboosts.com/" />
-      <meta
-        name="description"
-        content="RSBoosts.com - Cheap & reliable OSRS boosting service with fast delivery. We're a market leading company in the OSRS boosting market and we provide the best prices for Oldschool Runescape services. Contact us through our support or by email, or use the provided calculators on the site to look up the prices."
-      />
+      <meta name="description" content="VGHI" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="theme-color" content="#363636" />
-      <meta property="og:title" content="RSBoosts | Cheap & Reliable" />
+      <meta property="og:title" content="Varför går hon inte | Frida Walter" />
       <meta property="og:type" content="web" />
       <meta property="og:url" content="https://www.rsboosts.com/" />
-      <meta
-        property="og:description"
-        content="RSBoosts.com - Cheap & reliable OSRS boosting service with fast delivery. We're a market leading company in the OSRS boosting market and we provide the best prices for Oldschool Runescape services. Contact us through our support or by email, or use the provided calculators on the site to look up the prices."
-      />
-      <meta
-        property="og:image"
-        content="https://www.rsboosts.com/RSBoosts-seo.png"
-      />
-      <meta property="og:site_name" content="RSBoosts" />
+      <meta property="og:description" content="VGHI" />
+      <meta property="og:image" content="https://www.rsboosts.com/bok1.jpg" />
+      <meta property="og:site_name" content="VGHI" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="https://www.rsboosts.com/" />
-      <meta name="twitter:title" content="RSBoosts | Cheap & Reliable" />
-      <meta
-        name="twitter:description"
-        content="RSBoosts.com - Cheap & reliable OSRS boosting service with fast delivery. We're a market leading company in the OSRS boosting market and we provide the best prices for Oldschool Runescape services. Contact us through our support or by email, or use the provided calculators on the site to look up the prices."
-      />
-      <meta
-        name="twitter:image"
-        content="https://www.rsboosts.com/RSBoosts-seo.png"
-      />
+      <meta name="twitter:title" content="Varför går hon inte | Frida Walter" />
+      <meta name="twitter:description" content="VGHI" />
+      <meta name="twitter:image" content="https://www.rsboosts.com/bok1.jpg" />
     </>
   );
 };
