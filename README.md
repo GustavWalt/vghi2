5d12a69b86d65bb871855fd288cf75b232b26914
GustavWalt <shardoomtv6957@hotmail.com>
2021-07-04 13:44:55 +0200

    Notifications/spinner for UI

--- README.md
+++ README.md
@@ -2,9 +2,6 @@
 
 # npx prisma studio - starting prisma studio to visualize data
 
-Skapa kontakt sida (eget API)
-
-Säkerhet på formulären.
-Kontrollera priset på böckerna i backend.
-Privacy policy/köpvillkor.
-Nästa knapp i din kundvagn
+Skapa kontakt sida (eget API, egen db tabell).
+Visa användaren att beställningen gick igenom.
+Privacy policy/köpvillkor/frågor och svar. (be Frida om det)
--- components/cart/Header.tsx
+++ components/cart/Header.tsx
@@ -1,7 +1,9 @@
 //React/Next/Styling stuff
-import React, { useReducer, useMemo } from "react";
+import React, { useReducer, useMemo, useState } from "react";
 import Image from "next/image";
 import styles from "../../style/modules/cart/Header.module.scss";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import "react-responsive-modal/styles.css";
 
 //API packages
 import axios from "axios";
@@ -10,16 +12,19 @@ import axios from "axios";
 import { useSelector, RootStateOrAny, useDispatch } from "react-redux";
 import { removeFromCart, addToCart } from "../../redux/cart";
 
-//External packages (form/notifications)
+//External packages (form/notifications/spinner/modal)
 import { useToasts } from "react-toast-notifications";
 import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
+import Loader from "react-loader-spinner";
+import { Modal } from "react-responsive-modal";
 
 //Components
 import GridItem from "../assets/GridItem";
 import H1 from "../assets/H1";
 import Fade from "../assets/Fade";
 //---------------------------------------------------------
+
 // Assigning type-safe for the form data.
 interface FormTypes {
   email: string;
@@ -70,7 +75,20 @@ const formReducer = (state, event) => {
   };
 };
 
+// Get current date
+var rightNow = new Date();
+var dateRightNow = rightNow.toISOString().slice(0, 10).replace(/-/g, "/");
+
 const Header = () => {
+  //States
+  const [checkingOut, setCheckingOut] = useState(false);
+
+  const [openModal, setOpenModal] = useState(false);
+  const onOpenModal = () => setOpenModal(true);
+  const onCloseModal = () => setOpenModal(false);
+
+  const [modalData, setModalData] = useState({});
+
   // useReducer hook to set/get formData.
   const [formData, setFormData] = useReducer(formReducer, {});
 
@@ -100,6 +118,10 @@ const Header = () => {
 
   // Function for checkout, runs when you submit the form.
   const checkout = async (props) => {
+    // Display spinner
+    setCheckingOut(true);
+    document.body.classList.toggle("overflow-hidden");
+
     // Making API request to /api/questing to post the order.
     const response = await axios.post("/api/checkout", {
       email: props.email,
@@ -110,8 +132,18 @@ const Header = () => {
       zip: props.zip,
       order: finalCart,
     });
+
+    //Remove spinner
+    setCheckingOut(false);
+    document.body.classList.toggle("overflow-hidden");
+
     // Logging response
-    console.log("API RESPONSE ", response.status);
+    if (response.status === 201) {
+      //Visa att köpet gick igenom
+      setOpenModal(true);
+    } else {
+      setOpenModal(false);
+    }
   };
 
   var finalCart = [
@@ -138,176 +170,222 @@ const Header = () => {
   finalCart[1].totalPrice = finalCart[1].amount * 149;
   // Frontend
   return (
-    <div className={`${styles.header}`}>
-      <div className={styles.cart}>
-        <H1 title="Steg 1 - Din Kundvagn" />
-        <div className="flex">
-          {finalCart.map((item) => (
-            <>
-              {item.amount > 0 ? (
-                <div
-                  className={`flexCol ${styles.bookItem}`}
-                  key={Date.now().toString()}
-                >
-                  <Image src={item.img} alt="me" width="350" height="200" />
-                  <div className={styles.bookData}>
-                    <h1>{item.name}</h1>
-                    <p>
-                      <i>Frida Walter</i>
-                    </p>
-                    <hr />
-                    <div className={` ${styles.price}`}>
-                      <span
-                        className={styles.plusMinus}
-                        onClick={() => {
-                          dispatch(removeFromCart(item.name));
-                          const notification = () => {
-                            addToast(
-                              item.name + "  är nu borttagen i kundvagnen.",
-                              {
-                                appearance: "error",
-                                autoDismiss: true,
-                              }
+    <>
+      {checkingOut && (
+        <div className={styles.loader}>
+          <Loader
+            visible={true}
+            type="Puff"
+            color="#00BFFF"
+            height={100}
+            width={100}
+          />
+        </div>
+      )}
+
+      <Modal open={openModal} onClose={onCloseModal} center>
+        {console.log(modalData)}
+        <h2>Tack för ditt köp {modalData.name}!</h2>
+        <p>Fraktsätt: Postpaket</p>
+        <p>Betalning: Faktura</p>
+        <p>Beställningsdatum: {dateRightNow}</p>
+        <br />
+        <p>Adressuppgifter:</p>
+        <p>Namn: {modalData.name}</p>
+        <p>Mail: {modalData.email}</p>
+        <p>Telefonnummer: {modalData.phone}</p>
+        <p>Adress: {modalData.address}</p>
+        <p>Postnummer: {modalData.zip}</p>
+        <p>Ort: {modalData.neighbourhood}</p>
+        <br />
+        <p>Beställning:</p>
+        {finalCart.map((item) => (
+          <>
+            <p>
+              {item.name}: {item.amount}
+            </p>
+          </>
+        ))}
+        <br />
+        <p>
+          Vid några funderingar tveka inte att kontakta mig på vghi@gmail.com
+        </p>
+        <button onClick={onCloseModal}>Stäng</button>
+      </Modal>
+
+      <div className={`${styles.header}`}>
+        <div className={styles.cart}>
+          <H1 title="Steg 1 - Din Kundvagn" />
+          <div className="flex">
+            {finalCart.map((item) => (
+              <>
+                {item.amount > 0 ? (
+                  <div
+                    className={`flexCol ${styles.bookItem}`}
+                    key={Date.now().toString()}
+                  >
+                    <Image src={item.img} alt="me" width="350" height="200" />
+                    <div className={styles.bookData}>
+                      <h1>{item.name}</h1>
+                      <p>
+                        <i>Frida Walter</i>
+                      </p>
+                      <hr />
+                      <div className={` ${styles.price}`}>
+                        <span
+                          className={styles.plusMinus}
+                          onClick={() => {
+                            dispatch(removeFromCart(item.name));
+                            const notification = () => {
+                              addToast(
+                                item.name + "  är nu borttagen i kundvagnen.",
+                                {
+                                  appearance: "error",
+                                  autoDismiss: true,
+                                }
+                              );
+                            };
+                            notification();
+                          }}
+                        >
+                          -
+                        </span>
+                        <span>Antal:{item.amount}</span>
+                        <span
+                          className={styles.plusMinus}
+                          onClick={() => {
+                            dispatch(
+                              addToCart({ name: item.name, price: 149 })
                             );
-                          };
-                          notification();
-                        }}
-                      >
-                        -
-                      </span>
-                      <span>Antal:{item.amount}</span>
-                      <span
-                        className={styles.plusMinus}
-                        onClick={() => {
-                          dispatch(addToCart({ name: item.name, price: 149 }));
-                          const notification = () => {
-                            addToast(
-                              item.name + "  är nu tillagd i kundvagnen.",
-                              {
-                                appearance: "success",
-                                autoDismiss: true,
-                              }
-                            );
-                          };
-                          notification();
-                        }}
-                      >
-                        +
-                      </span>
-                      <br />
-                      <span>{item.totalPrice}kr</span>
+                            const notification = () => {
+                              addToast(
+                                item.name + "  är nu tillagd i kundvagnen.",
+                                {
+                                  appearance: "success",
+                                  autoDismiss: true,
+                                }
+                              );
+                            };
+                            notification();
+                          }}
+                        >
+                          +
+                        </span>
+                        <br />
+                        <span>{item.totalPrice}kr</span>
+                      </div>
                     </div>
                   </div>
-                </div>
-              ) : (
-                ""
-              )}
-            </>
-          ))}
+                ) : (
+                  ""
+                )}
+              </>
+            ))}
+          </div>
+          <h2>
+            <b>Att betala inkl moms: {itemTotal}kr</b>
+          </h2>
+          <hr />
         </div>
-        <h2>
-          <b>Att betala inkl moms: {itemTotal}kr</b>
-        </h2>
-        <hr />
+        <div className={styles.payment}>
+          <H1 title="Steg 2 - Leveransuppgifter" />
+          <Fade>
+            <Formik
+              initialValues={{
+                name: "",
+                email: "",
+                phone: "",
+                address: "",
+                zip: "",
+                neighbourhood: "",
+              }}
+              validationSchema={schema}
+              onSubmit={(values) => {
+                setModalData(values);
+                checkout(values);
+              }}
+            >
+              {({ errors, touched }) => (
+                <Form className={styles.form}>
+                  <label htmlFor="name">Fullständigt namn</label>
+                  <Field
+                    className={`${styles.input} ${
+                      errors.name && touched.name ? styles.inputError : ""
+                    }`}
+                    name="name"
+                    placeholder="Förnamn Efternamn"
+                  />
+                  {errors.name && touched.name ? (
+                    <div className={styles.error}>{errors.name}</div>
+                  ) : null}
+                  <label htmlFor="email">Email</label>
+                  <Field
+                    className={`${styles.input} ${
+                      errors.email && touched.email ? styles.inputError : ""
+                    }`}
+                    name="email"
+                    type="email"
+                    placeholder="Exempel@gmail.com"
+                  />
+                  {errors.email && touched.email ? (
+                    <div className={styles.error}>{errors.email}</div>
+                  ) : null}
+                  <label htmlFor="phone">Telefonnummer</label>
+                  <Field
+                    className={`${styles.input} ${
+                      errors.phone && touched.phone ? styles.inputError : ""
+                    }`}
+                    name="phone"
+                    placeholder="07X-XXX XX XX"
+                  />
+                  {errors.phone && touched.phone ? (
+                    <div className={styles.error}>{errors.phone}</div>
+                  ) : null}
+                  <label htmlFor="address">Adress</label>
+                  <Field
+                    className={`${styles.input} ${
+                      errors.address && touched.address ? styles.inputError : ""
+                    }`}
+                    name="address"
+                    placeholder="Gatan 87"
+                  />
+                  {errors.address && touched.address ? (
+                    <div className={styles.error}>{errors.address}</div>
+                  ) : null}
+                  <label htmlFor="zip">Postnummer</label>
+                  <Field
+                    className={`${styles.input} ${
+                      errors.zip && touched.zip ? styles.inputError : ""
+                    }`}
+                    name="zip"
+                    placeholder="XXXXX"
+                  />
+                  {errors.zip && touched.zip ? (
+                    <div className={styles.error}>{errors.zip}</div>
+                  ) : null}
+                  <label htmlFor="neighbourhood">Ort</label>
+                  <Field
+                    className={`${styles.input} ${
+                      errors.neighbourhood && touched.neighbourhood
+                        ? styles.inputError
+                        : ""
+                    }`}
+                    name="neighbourhood"
+                    placeholder="Ort"
+                  />
+                  {errors.neighbourhood && touched.neighbourhood ? (
+                    <div className={styles.error}>{errors.neighbourhood}</div>
+                  ) : null}
+                  <button className={styles.checkoutBtn} type="submit">
+                    Submit
+                  </button>
+                </Form>
+              )}
+            </Formik>
+          </Fade>
+        </div>
       </div>
-      <div className={styles.payment}>
-        <H1 title="Steg 2 - Leveransuppgifter" />
-        <Fade>
-          <Formik
-            initialValues={{
-              name: "",
-              email: "",
-              phone: "",
-              address: "",
-              zip: "",
-              neighbourhood: "",
-            }}
-            validationSchema={schema}
-            onSubmit={(values) => {
-              checkout(values);
-              console.log(values);
-            }}
-          >
-            {({ errors, touched }) => (
-              <Form className={styles.form}>
-                <label htmlFor="name">Fullständigt namn</label>
-                <Field
-                  className={`${styles.input} ${
-                    errors.name && touched.name ? styles.inputError : ""
-                  }`}
-                  name="name"
-                  placeholder="Förnamn Efternamn"
-                />
-                {errors.name && touched.name ? (
-                  <div className={styles.error}>{errors.name}</div>
-                ) : null}
-                <label htmlFor="email">Email</label>
-                <Field
-                  className={`${styles.input} ${
-                    errors.email && touched.email ? styles.inputError : ""
-                  }`}
-                  name="email"
-                  type="email"
-                  placeholder="Exempel@gmail.com"
-                />
-                {errors.email && touched.email ? (
-                  <div className={styles.error}>{errors.email}</div>
-                ) : null}
-                <label htmlFor="phone">Telefonnummer</label>
-                <Field
-                  className={`${styles.input} ${
-                    errors.phone && touched.phone ? styles.inputError : ""
-                  }`}
-                  name="phone"
-                  placeholder="07X-XXX XX XX"
-                />
-                {errors.phone && touched.phone ? (
-                  <div className={styles.error}>{errors.phone}</div>
-                ) : null}
-                <label htmlFor="address">Adress</label>
-                <Field
-                  className={`${styles.input} ${
-                    errors.address && touched.address ? styles.inputError : ""
-                  }`}
-                  name="address"
-                  placeholder="Gatan 87"
-                />
-                {errors.address && touched.address ? (
-                  <div className={styles.error}>{errors.address}</div>
-                ) : null}
-                <label htmlFor="zip">Postnummer</label>
-                <Field
-                  className={`${styles.input} ${
-                    errors.zip && touched.zip ? styles.inputError : ""
-                  }`}
-                  name="zip"
-                  placeholder="XXXXX"
-                />
-                {errors.zip && touched.zip ? (
-                  <div className={styles.error}>{errors.zip}</div>
-                ) : null}
-                <label htmlFor="neighbourhood">Ort</label>
-                <Field
-                  className={`${styles.input} ${
-                    errors.neighbourhood && touched.neighbourhood
-                      ? styles.inputError
-                      : ""
-                  }`}
-                  name="neighbourhood"
-                  placeholder="Ort"
-                />
-                {errors.neighbourhood && touched.neighbourhood ? (
-                  <div className={styles.error}>{errors.neighbourhood}</div>
-                ) : null}
-                <button className={styles.checkoutBtn} type="submit">
-                  Submit
-                </button>
-              </Form>
-            )}
-          </Formik>
-        </Fade>
-      </div>
-    </div>
+    </>
   );
 };
 
